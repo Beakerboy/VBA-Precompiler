fb6450c370cc1b45001e3d0fb601c348413e26da
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-02-08 14:32:29 -0500

    Create precompiler_listener.py

---
+++ src/vba_precompiler/precompiler_listener.py
@@ -0,0 +1,40 @@
+from vba_precompiler.vba_ccVisitor import vba_ccVisitor
+
+
+class PrecompilerVisitor(vba_ccVisitor)
+
+    def visitOpExpr(self: T, ctx) -> int:
+        left = visit(ctx.left)
+        right = visit(ctx.right)
+        op = ctx.op.getText()
+        switch (op.charAt(0)) {
+            case '*': return left * right
+            case '/': return left / right
+            case '+': return left + right
+            case '-': return left - right
+            default: raise Exception("Unknown operator " + op)
+
+    def visitRelExpr(self: T, ctx) -> bool:
+        left = visit(ctx.left)
+        right = visit(ctx.right)
+        op = ctx.op.getText()
+        switch (op.charAt(0)) {
+            case '<': return left < right
+            case '>': return left > right
+            case '=': return left == right
+            case '<>': pass
+            case '><': return left != right
+            case '>=': pass
+            case '=>': return left >= right
+            case '<=': pass
+            case '=<': return left <= right
+            default: raise Exception("Unknown operator " + op)
+    def visitStartRule(self: T, ctx) -> None:
+        'visit each block
+        return this.visit(ctx.expr());
+
+    def visitAtomExpr(self: T, ctx) -> Any:
+        return Integer.valueOf(ctx.getText());
+
+    def visitParenExpr(self: T, ctx) -> Any:
+        return self.visit(ctx.expr())
