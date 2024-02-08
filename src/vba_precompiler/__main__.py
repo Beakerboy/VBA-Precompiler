676466e4afcd620b3fc5e2a12b2579b4472956da
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-02-08 14:50:46 -0500

    Update __main__.py

--- src/vba_precompiler/__main__.py
+++ src/vba_precompiler/__main__.py
@@ -8,28 +8,28 @@ def main() -> None:
     parser.add_argument("-v", "--version", default=6,
                         help="VBA version, 6 or 7")
     args = parser.parse_args()
-    Win16 = False
-    Win32 = False
-    Win64 = False
-    Mac = False
-    Vba6 = False
-    Vba7 = False
+    win16 = False
+    win32 = False
+    win64 = False
+    mac = False
+    vba6 = False
+    vba7 = False
     if args.os == "Mac":
-        Mac = True
+        mac = True
     elif args.os == "Win16":
-        Win16 = True
+        win16 = True
     elif args.os == "Win22":
-        Win32 = True
+        win32 = True
     elif args.os == "Win16":
-        Win32 = True
-        Win64 = True
+        win32 = True
+        win64 = True
     else:
         raise Exception("version unsupported")
     if args.version == 6:
-        if Win32 or Mac:
-            Vba6 = True
+        if win32 or mac:
+            vba6 = True
     elif args.version == 7:
-        Vba7 = True
+        vba7 = True
     else:
         raise Exception("Version unsupported")
     
