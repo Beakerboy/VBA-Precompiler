56c6da54bd91f928b43f57cc8028a64fa217d9f4
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-02-11 19:38:36 -0500

    Update compiler_visitor.py

--- src/vba_precompiler/compiler_visitor.py
+++ src/vba_precompiler/compiler_visitor.py
@@ -154,6 +154,7 @@ class PrecompilerVisitor(vba_ccVisitor):
             self: T,
             ctx: Parser.LiteralExpressContext
     ) -> Any:
+        raise Exception(ctx.start.text)
         if ctx.start.type == Lexer.BOOLEANLITERAL:
             return ctx.start.text.upper() == 'TRUE'
         return 0
