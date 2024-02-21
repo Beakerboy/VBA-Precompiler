609de83542357662dbdd39587783e3f5665e3cb9
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-02-21 12:34:52 -0500

    Create test_visitor.py

---
+++ tests/Unit/test_visitor.py
@@ -0,0 +1,5 @@
+from vba_precompiler.precompiler_visitor import PrecompilerVisitor as Visitor
+def test_split_nl() -> None:
+    tok_txt = "\r\n\n\r"
+    result = Visitor.split_nl(tok_text)
+    assert result == ["\r\n", "\n", "\r"]
